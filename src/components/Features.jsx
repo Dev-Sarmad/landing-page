06a2a9a2c4e1d5bc022399b83360be7a13004c74
Dev-Sarmad <sarmadh230@gmail.com>
2023-06-23 20:32:37 +0500

    features section

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,37 +1,40 @@
-import React from 'react'
-
+import React from "react";
+import transaction from "../assets/transaction.png";
+import trade from "../assets/trade.png";
+import app from "../assets/mobileapp.png";
 function Features() {
   return (
     <>
-    <div className='container mx-auto mt-28 flex flex-col spaxex-x-0 space-y-4'>
-    <div className=" flex flex-col md:flex-row px-3 md:items-center  justify-evenly">
+      <div className="container mx-auto mt-28 flex flex-col spaxex-x-0 space-y-4">
+        <div className=" flex flex-col md:flex-row   ">
           <div>
-            <h1 className="text-blacklight text-4xl md:text-5xl font-bold">
-              The Best Lightning <br /> cryptyo
-              <img
-                src={transaction}
-                className="w-8 inline"
-                alt=""
-              /> trading{" "}
+            <h1 className="text-blacklight text-4xl md:text-5xl font-bold text-left">
+              Best way to buy
+              <img src={transaction} className="w-8 inline" alt="" /> <br />
+            </h1>
+            <h1 className="text-blacklight text-4xl md:text-5xl font-bold ml-32 mt-5 px-3">
+              trade <img src={trade} alt="" className="inline" /> crypto
             </h1>
           </div>
-
+        </div>
+        <div className="flex flex-col space-y-3 space-x-0  md:flex-row md:space-x-3 md:space-y-0 items-center justify-around">
           <div>
-            <p className="text-detaillight mt-3">
-              Trusted by millions since 2010 with <br /> over $1 Trillion in
-              crypto transactions.
-            </p>
-            <div className="flex space-x-3 space-y-0 mt-3 ">
-              <button className="text-white rounded-lg px-4  bg-blacklight">
-                Get Started
-              </button>
-              <button className="text-blacklight text-xl">Explore</button>
+            <img src={app} alt="" className="" />
+          </div>
+          <div className="flex flex-col space-y-3 space-x-0">
+            <div className="text-left text-detaillight">
+              <p>
+                The world fastest crypto matching engine built by traders of
+                traders.
+              </p>
+              <p className="">Increased purchasing power means up to 5 times more profit.</p>
             </div>
+            
           </div>
         </div>
-    </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Features
+export default Features;
