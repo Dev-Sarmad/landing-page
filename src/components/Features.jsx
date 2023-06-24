8faba4daa8075dc3cf63893509de6d32f46e1a24
Dev-Sarmad <sarmadh230@gmail.com>
2023-06-24 10:43:28 +0500

    main feature

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import transaction from "../assets/transaction.png";
 import trade from "../assets/trade.png";
 import app from "../assets/mobileapp.png";
+import google from "../assets/google.png";
+import apple from "../assets/Apple-icon.png";
+import bitcoin from "../assets/bitcoin.svg";
+import ethereum from "../assets/ethereum.svg";
 function Features() {
   return (
     <>
@@ -27,9 +31,34 @@ function Features() {
                 The world fastest crypto matching engine built by traders of
                 traders.
               </p>
-              <p className="">Increased purchasing power means up to 5 times more profit.</p>
+              <p className="">
+                Increased purchasing power means up to 5 times more profit.
+              </p>
             </div>
-            
+            <div className="flex space-x-5 space-y-0 ">
+              <button className="flex items-center bg-blacklight text-white px-3 py-2 rounded-md font-bold">
+                <img src={google} alt="" />
+                Get app from
+                <br /> playstore
+              </button>
+              <button className="flex  items-center  bg-blacklight text-white px-3 py-2  rounded-md font-bold">
+                <img src={apple} alt="" />
+                Get app from <br />
+                App Store
+              </button>
+            </div>
+            <div className="flex flex-col space-y-3 space-x-0 md:flex-row md:space-x-3 md:space-y-0 items-center justify-evenly">
+              <div className="border-2 border-blacklight p-3 rounded-xl ">
+                <img src={bitcoin} alt="" />
+                <h1>Bitcoin BTC</h1>
+                <h3>$1098345 &nbsp; +2.234% </h3>
+              </div>
+              <div className="border-2 border-blacklight p-3 rounded-xl ">
+                <img src={ethereum} alt="" />
+                <h1>Ethereum ETC</h1>
+                <h3>$17458345 &nbsp; +0.234% </h3>
+              </div>
+            </div>
           </div>
         </div>
       </div>
